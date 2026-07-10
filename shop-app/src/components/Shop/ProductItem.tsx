fc376846345f2c3edc4bb10a76988ac18aabf95a
Shahrukh Hughes <s.khan_1987@hotmail.co.uk>
2026-07-10 12:55:52 +0100

    Fix wishlist persistence after page refresh

--- shop-app/src/components/Shop/ProductItem.tsx
+++ shop-app/src/components/Shop/ProductItem.tsx
@@ -1,26 +1,25 @@
-import { useAppDispatch } from "../../store/hooks";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
 
 import type { Product } from "../../types/product";
 
 import { cartActions } from "../../store/cart-slice";
 import { wishlistActions } from "../../store/wishlist-slice";
+
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+
 import classes from "./ProductItem.module.css";
 
 import { FaRegHeart, FaHeart, FaCartPlus } from "react-icons/fa";
-import { productsActions } from "../../store/products-slice";
 
 type ProductItemProps = Product;
 
-const ProductItem = ({
-  id,
-  title,
-  price,
-  image,
-  isFavourite,
-}: ProductItemProps) => {
+const ProductItem = ({ id, title, price, image }: ProductItemProps) => {
   const dispatch = useAppDispatch();
+
+  const wishlistItems = useAppSelector((state) => state.wishlist.items);
+
+  const isProductFavourite = wishlistItems.some((item) => item.id === id);
 
   const addToCartHandler = () => {
     dispatch(
@@ -33,24 +32,24 @@ const ProductItem = ({
     );
   };
 
-  const addToWishlistHandler = () => {
-    if (!isFavourite) {
-      dispatch(
-        wishlistActions.addItemToWishlist({
-          id,
-        }),
-      );
-      dispatch(productsActions.toggleFavourite(id));
-    } else {
+  const toggleWishlistHandler = () => {
+    if (isProductFavourite) {
       dispatch(wishlistActions.removeItemFromWishlist(id));
-      dispatch(productsActions.toggleFavourite(id));
+      return;
     }
+
+    dispatch(
+      wishlistActions.addItemToWishlist({
+        id,
+      }),
+    );
   };
 
   return (
     <Card className={classes.product}>
       <li className={classes.product__item}>
         <h3 className={classes.product__title}>{title}</h3>
+
         <figure className={classes["product__image-container"]}>
           <img
             src={image}
@@ -60,23 +59,30 @@ const ProductItem = ({
           />
           <figcaption id="product__image--caption">Product Image</figcaption>
         </figure>
+
         <div className={classes.product__price}>£{price.toFixed(2)}</div>
+
         <div className={classes.product__buttons}>
           <Button
             onClick={addToCartHandler}
-            aria-label="Button to add to Cart"
+            aria-label="Add to cart"
             tabIndex={1}
             title="Add to Cart"
           >
             <FaCartPlus className={classes.product__icon} title="Add to Cart" />
           </Button>
+
           <Button
-            onClick={addToWishlistHandler}
-            aria-label="Button to add to Wishlist"
+            onClick={toggleWishlistHandler}
+            aria-label={
+              isProductFavourite ? "Remove from wishlist" : "Add to wishlist"
+            }
             tabIndex={2}
-            title="Add to Wishlist"
+            title={
+              isProductFavourite ? "Remove from Wishlist" : "Add to Wishlist"
+            }
           >
-            {isFavourite ? (
+            {isProductFavourite ? (
               <FaHeart
                 className={classes.product__icon}
                 title="Remove from Wishlist"
